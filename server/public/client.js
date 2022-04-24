f845a6ae2cb91b4d66d6c46f828992ac540773f9
Madeline Prickette <madelineprickette@gmail.com>
2022-04-24 15:16:11 -0500

    successfully created a delete route from client to server to database and b ack as well as refreshed the list on the DOM with refreshTasks

--- server/public/client.js
+++ server/public/client.js
@@ -4,15 +4,22 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('JQ');
+    refreshTasks(); // This should load immediately so the user can see their tasks.
     $('#addTaskButton').on('click', addTask);
+    // Below click listeners are for the newly addded buttons. It says "hey document, make sure to listen for this,
+    // these buttons will appear later". the addTask button is populated with the DOM from the start.
+    $('#viewList').on('click', '.deleteButton', deleteTask);
+    // so the '#viewList is the location in which the deletion and button population will occour. The .deleteButton is seen 
+    // /identified wihtin the parenthesis.
 }
 
 function addTask() { // POST
     // Get info to send to the server
-    const taskWePost = {
+    let taskWePost = {
         task: $('#createATask').val(), 
         //call this taskWeAdd sometime?
     };$.ajax({
+        // This is teh package object that is sent to server
         method: 'POST',
         url: '/tasks',
         data: taskWePost
@@ -22,15 +29,42 @@ function addTask() { // POST
         console.log('error in the task post', error);       
     });
     refreshTasks();
-}
+}; // End addTask SUCCESSFULLY WORKS!!!!!!!!!!!
 
 function refreshTasks() {
     $.ajax({
         type: 'GET',
         url: '/tasks'
     }).then(function(response) {
+        let el = $('#viewList');
+        el.empty();
+        for (let i=0; i<response.length; i++){
+            el.append( `<li>${ response[i].task}
+            <button class="completeButton" data-id="${response[i].id}">Complete Task</button>
+            <button class="deleteButton" data-id="${response[i].id}">Delete</button>
+            </li>`)
+        }//end for loop for appending
         console.log(response);
     }).catch(function(error){
         console.log('error in client', error);
     });
-}
+}// End of refreshTasks a GET REQUEST it WORKS!!!!!!!
+
+// Wait we need a click listener.. but where...?
+function deleteTask() {
+    const id = $(this).data('id');
+    console.log('ok the delete button seems to be doing something... thats good', id); // console log WORKS!
+    // Now we need an ajax function because we are on the client side.
+    $.ajax({
+        type: 'DELETE',
+        url: `/tasks/${id}`
+        // This url says what exactly it is targeting. I think...
+    }).then( function(response){
+        console.log('sent a delete request to the server', response);
+        refreshTasks();
+        // This will update the current list on the DOM
+        // Now to handle errors gracefully...
+    }).catch( function(error){
+        console.log('there was an error when attempting to delete a task', error)
+    })
+};
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 //connection from pool.js
 const pool = require('./modules/pool');
+const { send } = require('express/lib/response');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,22 +12,63 @@ app.use(express.static('server/public'))
 
 
 app.post('/tasks', (req, res) => {
-    console.log('yay');
+    console.log('yay im in the post route');
+    // This is how we insert something into the database, what is in green can be tested in Postico to check validity
+    const query =  `INSERT INTO "tasks" ( "task" ) VALUES ($1);`;
+    // I think this is requesting to add to the database as res.send is responding to the client...?
+    const values = [req.body.task];
+    // Making a query
+    pool.query( query, values ).then( (results)=> {
+      // Send a status that says CREATED becacause the task was successfully added to the database
+      res.sendStatus(201);
+      // Handled the error gracefully so it sends an error in the DOM console log instead of blowing up and destroying shit. lol
+    }) .catch(error => {
+      console.log('there was an error with adding the new task to the database', error);
+      res.sendStatus(500);
+    })
     //res.send('yay now the post is in the console');
-})
+})// End of posting new tasks WORKS!!!!!!!!!!!!
 
 app.get('/tasks', (req, res) => {
-    console.log('omg the get route works');
+    console.log('omg the get route works', req.body);
     //res.send('omg the get route works in the DOM console');
     const queryGetTasks = 'SELECT * FROM "tasks" ORDER BY "id";';
+    // This is a query where we are getting information from the database
     pool.query(queryGetTasks).then(result => {
+      // This res.send is bringing back the data rows and sending it to the client
     res.send(result.rows);
-    })
+    })// Incase of problems, we handled it gracefully.
     .catch(error => {
       console.log('error retrieving/refreshing tasks', error);
+      // The status of 500 means there is a server end problem...
       res.sendStatus(500);
     });
-});
+});// End of get or refreshing tasks, SUCCESSFUL YAY!
+
+app.delete('/tasks/:id',  (req, res) => {
+  // I think the :id means the specific id attached to each task, 
+  // like it was referred in the client.js.. could be wrong.
+  console.log('the server understands the delete button was pushed', req.params.id);
+  // I need more clarification on req.params.id plz..
+  // DELETE FROM is instructing the databast to delete a specific task in a language it can understand.
+  const query = `DELETE FROM "tasks" WHERE "id"=$1;`;
+  const values = [req.params.id];
+  //sending a query to the database to relay information
+  pool.query( query, values ).then((response) => {
+    res.sendStatus(200);
+    // Handling error gracefully via 500 server code
+  }).catch((error) => {
+    console.log('there was a problem trying to delete this task from the database', error);
+    res.sendStatus(500);
+  })
+}); // End delete
+
+
+
+
+
+
+
 
 
 // Start listening for requests on a specific port
